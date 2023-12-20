0e5888fac3525bf21aadd8dc9ddbdcfa61a8bc61
Alberto <albertogil261@gmail.com>
2023-12-20 11:09:33 +0100

    actividades y layouts creados

--- app/src/main/java/com/example/comercial/Actividad_Partners.java
+++ app/src/main/java/com/example/comercial/Actividad_Partners.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Actividad_Partners extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_partners);
     }
 }
---
+++ app/src/main/java/com/example/comercial/Actividad_Presentacion.java
@@ -0,0 +1,14 @@
+package com.example.comercial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Actividad_Presentacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_presentacion);
+    }
+}
