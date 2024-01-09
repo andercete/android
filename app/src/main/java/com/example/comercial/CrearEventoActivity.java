b15af716c1e2ed72a8be24031bd2397e4b8f2ba9
BICHO\MIPC <inigovaldivia@gmail.com>
2024-01-09 12:29:13 +0100

    validacion

--- app/src/main/java/com/example/comercial/CrearEventoActivity.java
+++ app/src/main/java/com/example/comercial/CrearEventoActivity.java
@@ -1,13 +1,20 @@
 package com.example.comercial;
 // CrearEventoActivity.java
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CrearEventoActivity extends AppCompatActivity {
+
+    AlertDialog.Builder dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,20 @@ public class CrearEventoActivity extends AppCompatActivity {
                 String ubicacion = ubicacionEditText.getText().toString();
                 String descripcion = descripcionEditText.getText().toString();
 
+
+
+                if(tituloEditText.getText().toString().isEmpty()){
+
+                    mostrarError("Debes de poner nombre al evento", tituloEditText);
+                    return;
+                }else if
+
+                (fechaEditText.getText().toString().isEmpty()){
+                    mostrarError("Debes Rellenar la fecha del evento", tituloEditText);
+                    return;
+                }
+
+
                 // Enviar los datos del nuevo evento de vuelta a la actividad principal
                 Intent resultadoIntent = new Intent();
                 resultadoIntent.putExtra("titulo", titulo);
@@ -40,5 +61,26 @@ public class CrearEventoActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+
+
+
     }
+
+    private void mostrarError(String mensaje, final EditText editText) {
+        dialog = new AlertDialog.Builder(CrearEventoActivity.this);
+        dialog.setTitle("Error");
+        dialog.setMessage(mensaje);
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("Cerrar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogo, int id) {
+                dialogo.cancel();
+                editText.requestFocus();
+            }
+        });
+        dialog.show();
+    }
+
 }
