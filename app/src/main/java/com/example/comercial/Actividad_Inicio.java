80aa175630f57eda0a66c8c8af73e850642df016
Ander <andercete@gmail.com>
2024-02-08 14:07:12 +0100

    no message

--- app/src/main/java/com/example/comercial/Actividad_Inicio.java
+++ app/src/main/java/com/example/comercial/Actividad_Inicio.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -19,6 +20,9 @@ import com.example.comercial.partners.Partner;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.xmlpull.v1.XmlSerializer;
@@ -28,6 +32,7 @@ import com.example.comercial.calendario.Actividad_Eventos;
 import com.example.comercial.partners.Actividad_Partners;
 import com.example.comercial.Catalogo.Actividad_Catalogo;
 import java.util.ArrayList;
+import java.util.Locale;
 
 // implements OnMapReadyCallback
 public class Actividad_Inicio extends AppCompatActivity {
@@ -43,14 +48,7 @@ public class Actividad_Inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_inicio);
 
-        /* Google Maps
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        try {
-            mapFragment.getMapAsync(this);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Error al cargar el mapa", Toast.LENGTH_SHORT).show();
-        }*/
+
 
         bCitas = findViewById(R.id.bAgenda);
         bPartner = findViewById(R.id.bPresentacionPartners);
@@ -82,7 +80,163 @@ public class Actividad_Inicio extends AppCompatActivity {
             }
         });
 
-        /*bDelegacion.setOnClickListener(new View.OnClickListener() {
+
+        bDelegacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Partner> partnersDeHoy = db.getPartnersOfToday();
+                if (partnersDeHoy.isEmpty()) {
+                    Toast.makeText(Actividad_Inicio.this, "No hay partners para enviar hoy.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String nombreArchivo = "partners_hoy.xml";
+                generarArchivoXML(partnersDeHoy, nombreArchivo);
+
+                File archivoXml = new File(getFilesDir(), "partners/"+nombreArchivo);
+                Uri uriArchivoXml = FileProvider.getUriForFile(
+                        Actividad_Inicio.this,
+                        "com.example.comercial.fileprovider",
+                        archivoXml);
+                ArrayList<Uri> archivosAdjuntos = new ArrayList<>();
+                archivosAdjuntos.add(uriArchivoXml);
+
+                Intent emailIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+                emailIntent.setType("text/xml");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"DelegacionGuipuzcoa@gem.com"}); // Cambia esto por la dirección real de correo electrónico de la delegación.
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Partners y pedidos de " + Metodos.obtenerFechaActual());
+                emailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, archivosAdjuntos);
+                emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Envío de partners y pedidos"));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(Actividad_Inicio.this, "No hay cliente de correo electrónico instalado.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        bTelefono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentTelefono = new Intent(Intent.ACTION_DIAL);
+                intentTelefono.setData(Uri.parse("tel:+34 635985923"));
+                startActivity(intentTelefono);
+            }
+        });
+
+        bEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String para = "andercete@gmail.com";
+
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse("mailto:" + para));
+
+                try {
+                    startActivity(emailIntent);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Metodos.lanzarToast("Ha habido un error al intentar abrir el correo electrónico.",Actividad_Inicio.this);
+                }
+            }
+
+        });
+    }
+    public void generarArchivoXML(List<Partner> partners, String nombreArchivo) {
+        // Construimos el nombre del archivo con la fecha de hoy
+
+        File directorioPartners = new File(getFilesDir(), "partners");
+        if (!directorioPartners.exists()) {
+            directorioPartners.mkdirs();
+        }
+
+        File archivoXml = new File(directorioPartners, nombreArchivo);
+
+        try (FileOutputStream fos = new FileOutputStream(archivoXml)) {
+            XmlSerializer serializer = Xml.newSerializer();
+            StringWriter writer = new StringWriter();
+            serializer.setOutput(writer);
+
+            serializer.startDocument("UTF-8", true);
+            serializer.startTag("", "Partners");
+
+            for (Partner partner : partners) {
+                serializer.startTag("", "Partner");
+
+                serializer.startTag("", "IdZona");
+                serializer.text(String.valueOf(partner.getIdZona()));
+                serializer.endTag("", "IdZona");
+
+                serializer.startTag("", "Nombre");
+                serializer.text(partner.getNombre());
+                serializer.endTag("", "Nombre");
+
+                serializer.startTag("", "CIF");
+                serializer.text(partner.getCif());
+                serializer.endTag("", "CIF");
+
+                serializer.startTag("", "Direccion");
+                serializer.text(partner.getDireccion());
+                serializer.endTag("", "Direccion");
+
+                serializer.startTag("", "Telefono");
+                serializer.text(partner.getTelefono());
+                serializer.endTag("", "Telefono");
+
+                serializer.startTag("", "Correo");
+                serializer.text(partner.getCorreo());
+                serializer.endTag("", "Correo");
+
+                serializer.startTag("", "FechaRegistro");
+                serializer.text(partner.getFechaRegistro());
+                serializer.endTag("", "FechaRegistro");
+
+                serializer.endTag("", "Partner");
+            }
+
+            serializer.endTag("", "Partners");
+            serializer.endDocument();
+
+            // Aquí solo necesitas escribir el contenido una vez
+            fos.write(writer.toString().getBytes());
+            // No necesitas llamar a fos.close() aquí, el try-with-resources se encarga de ello
+        } catch (Exception e) {
+            Log.e("XMLGenerationError", "Error generando el archivo XML", e);
+        }
+    }
+
+
+
+    protected void onDestroy() {
+        if (db != null) {
+            db.close();
+        }
+        super.onDestroy();
+    }
+  /*  @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        // this.mMap.setOnMapClickListener(this);
+        // this.mMap.setOnMapLongClickListener(this);
+
+        LatLng donosti = new LatLng(43.30419712367967, -2.0165662074674695);
+        mMap.addMarker(new MarkerOptions().position(donosti).title("Gurmet Euskadi Market"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(donosti));
+
+    }*/
+
+    /* Google Maps
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        try {
+            mapFragment.getMapAsync(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error al cargar el mapa", Toast.LENGTH_SHORT).show();
+        }*/
+
+       /*bDelegacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean existePedido = true;
@@ -150,109 +304,4 @@ public class Actividad_Inicio extends AppCompatActivity {
             }
 
         });*/
-        bDelegacion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                List<Partner> partnersDeHoy = db.getPartnersOfToday();
-                if (partnersDeHoy.isEmpty()) {
-                    Toast.makeText(Actividad_Inicio.this, "No hay partners para enviar hoy.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                String nombreArchivo = "partners/partners_hoy.xml";
-                generarArchivoXML(partnersDeHoy, nombreArchivo);
-
-                File archivoXml = new File(getFilesDir(), nombreArchivo);
-                Uri uriArchivoXml = FileProvider.getUriForFile(Actividad_Inicio.this, "com.example.comercial.fileprovider", archivoXml);
-                ArrayList<Uri> archivosAdjuntos = new ArrayList<>();
-                archivosAdjuntos.add(uriArchivoXml);
-
-                Intent emailIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
-                emailIntent.setType("text/xml");
-                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"DelegacionGuipuzcoa@gem.com"}); // Cambia esto por la dirección real de correo electrónico de la delegación.
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Partners y pedidos de " + Metodos.obtenerFechaActual());
-                emailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, archivosAdjuntos);
-                emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-                try {
-                    startActivity(Intent.createChooser(emailIntent, "Envío de partners y pedidos"));
-                } catch (android.content.ActivityNotFoundException ex) {
-                    Toast.makeText(Actividad_Inicio.this, "No hay cliente de correo electrónico instalado.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-
-
-        bTelefono.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentTelefono = new Intent(Intent.ACTION_DIAL);
-                intentTelefono.setData(Uri.parse("tel:+34 635985923"));
-                startActivity(intentTelefono);
-            }
-        });
-
-        bEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String para = "DelegacionGuipuzcoa@gem.com";
-
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
-                emailIntent.setData(Uri.parse("mailto:" + para));
-
-                try {
-                    startActivity(emailIntent);
-                } catch (android.content.ActivityNotFoundException ex) {
-                    Metodos.lanzarToast("Ha habido un error al intentar abrir el correo electrónico.",Actividad_Inicio.this);
-                }
-            }
-
-        });
-    }
-    private void generarArchivoXML(List<Partner> partners, String nombreArchivo) {
-        try {
-            File archivoXml = new File(getFilesDir(), nombreArchivo);
-            FileOutputStream fos = new FileOutputStream(archivoXml, false);
-            XmlSerializer serializer = Xml.newSerializer();
-            serializer.setOutput(fos, "UTF-8");
-            serializer.startDocument(null, Boolean.TRUE);
-            serializer.setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true);
-            serializer.startTag(null, "Partners");
-
-            for (Partner partner : partners) {
-                serializer.startTag(null, "Partner");
-                // Repite este patrón para cada atributo de Partner
-                serializer.startTag(null, "Nombre");
-                serializer.text(partner.getNombre());
-                serializer.endTag(null, "Nombre");
-                // Continúa con los demás campos...
-                serializer.endTag(null, "Partner");
-            }
-
-            serializer.endTag(null, "Partners");
-            serializer.endDocument();
-            serializer.flush();
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    protected void onDestroy() {
-        if (db != null) {
-            db.close();
-        }
-        super.onDestroy();
-    }
-  /*  @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-        // this.mMap.setOnMapClickListener(this);
-        // this.mMap.setOnMapLongClickListener(this);
-
-        LatLng donosti = new LatLng(43.30419712367967, -2.0165662074674695);
-        mMap.addMarker(new MarkerOptions().position(donosti).title("Gurmet Euskadi Market"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(donosti));
-
-    }*/
 }
