fae30900856ad686b9b911611da5240ff1659cff
BICHO\MIPC <inigovaldivia@gmail.com>
2024-02-12 11:02:33 +0100

    pikete italiano

--- app/src/main/java/com/example/comercial/calendario/Actividad_AltaEvento.java
+++ app/src/main/java/com/example/comercial/calendario/Actividad_AltaEvento.java
@@ -1,11 +1,13 @@
 package com.example.comercial.calendario;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import com.example.comercial.BBDD.DbHelper;
 import com.example.comercial.BBDD.Evento;
 import com.example.comercial.R;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -23,8 +26,8 @@ public class Actividad_AltaEvento extends AppCompatActivity {
     private EditText editTextTitle, editTextLocation, editTextDate, editTextTime, editTextDescription;
     private Button buttonGuardar, buttonLimpiar;
     private DbHelper dbHelper;
-
     private Calendar calendar;
+    private SimpleDateFormat timeFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +41,18 @@ public class Actividad_AltaEvento extends AppCompatActivity {
         editTextTime = findViewById(R.id.horaEditText2);
         editTextDescription = findViewById(R.id.descripcionEditText);
 
-
         editTextDate = findViewById(R.id.editTextDate2);
         editTextDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePickerDialog();
+            }
+        });
+
+        editTextTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
             }
         });
 
@@ -55,7 +64,6 @@ public class Actividad_AltaEvento extends AppCompatActivity {
             }
         });
 
-
         buttonLimpiar = findViewById(R.id.button3);
         buttonLimpiar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +71,8 @@ public class Actividad_AltaEvento extends AppCompatActivity {
                 limpiarControles();
             }
         });
+
+        timeFormat = new SimpleDateFormat("HH:mm", Locale.US);
 
         calendar = Calendar.getInstance();
     }
@@ -76,7 +86,6 @@ public class Actividad_AltaEvento extends AppCompatActivity {
                         calendar.set(Calendar.YEAR, year);
                         calendar.set(Calendar.MONTH, monthOfYear);
                         calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
                         updateEditTextDate();
                     }
                 },
@@ -87,12 +96,40 @@ public class Actividad_AltaEvento extends AppCompatActivity {
         datePickerDialog.show();
     }
 
+    private void showTimePickerDialog() {
+        Calendar currentTime = Calendar.getInstance();
+        int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = currentTime.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+
+                        updateEditTextTime();
+                    }
+                },
+                hour,
+                minute,
+                true);
+
+        timePickerDialog.show();
+    }
+
+    private void updateEditTextTime() {
+        editTextTime.setText(timeFormat.format(calendar.getTime()));
+    }
+
     private void updateEditTextDate() {
         String myFormat = "yyyy-MM-dd"; // Cambia el formato de la fecha según tus necesidades
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
         editTextDate.setText(sdf.format(calendar.getTime()));
     }
+
     private void guardarEvento() {
         String title = editTextTitle.getText().toString().trim();
         String location = editTextLocation.getText().toString().trim();
@@ -100,7 +137,7 @@ public class Actividad_AltaEvento extends AppCompatActivity {
         String time = editTextTime.getText().toString().trim();
         String description = editTextDescription.getText().toString().trim();
 
-        if (!title.isEmpty() && !date.isEmpty() && !time.isEmpty() && !description.isEmpty()) {
+        if (!title.isEmpty() && !date.isEmpty() && isValidTime(time) && !description.isEmpty()) {
             Evento evento = new Evento(0, title, location, date, time, description);
             dbHelper.addEvento(evento);
 
@@ -108,19 +145,24 @@ public class Actividad_AltaEvento extends AppCompatActivity {
             setResult(RESULT_OK);
             finish();
         } else {
-            Toast.makeText(this, "Rellene todos los campos.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Rellene todos los campos correctamente.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void limpiarControles(){
+    private boolean isValidTime(String time) {
+        try {
+            timeFormat.parse(time);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
 
+    private void limpiarControles() {
         editTextTitle.setText("");
         editTextLocation.setText("");
         editTextTime.setText("");
         editTextDate.setText("");
         editTextDescription.setText("");
-
-
-        editTextDate = findViewById(R.id.editTextDate2);
     }
 }
